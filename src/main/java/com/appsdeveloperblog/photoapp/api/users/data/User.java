6655015cb36c7ffc11d523c99b3781d3b00e237c
Minh Tran <tranminhthongdn1988@gmail.com>
2023-10-14 13:30:33 -0500

    init commit

---
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/data/User.java
@@ -0,0 +1,22 @@
+package com.appsdeveloperblog.photoapp.api.users.data;
+
+public class User {
+    private String name;
+    private String username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+}
